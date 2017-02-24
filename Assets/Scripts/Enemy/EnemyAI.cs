268b0915a044f781a624563ef5a27baf9882280a
Michael Rivet <michaelrivet9.81@gmail.com>
2017-02-24 15:16:48 -0600

    Enemy AI Update.
    Correcting main branch for enemy AI.  Correcting dodge logic, adding
    random move logic when enemy is in shoot range.

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -9,25 +10,33 @@ public class EnemyAI : MonoBehaviour
     public Transform target;//set target from inspector instead of looking in Update
     Quaternion enemyRotation;
     Vector2 playerPos, enemyPos;
-    float speed = 3f;
+    public float Speed = 3f;
+    public float DodgeSpeed = 10f;
 
 	public float ShootDistance = 15f;
 	public float AggroDistance = 100f;
 	private float _lastShotTime;
 	private float _lastDodgeTime = 0f;
+    private float _lastMoveTime = 0f;
 	private float _distanceToPlayer;
-	private Vector2 _dodgeDirection;
+
+    private Vector2 _dodgeDirection;
+    private Vector2 _moveLocation;
+
+    // State bools
 	private bool _dodging = false;
+    private bool _moving = false;
+    private bool _shooting = false;
 
 	private SpriteRenderer _ren;			// Reference to the sprite renderer.
-	private LineRenderer _debugLine;
-
+	//private LineRenderer _debugLine;
+    
     void Start()
     {
         enemyRotation = this.transform.localRotation;
 		target = GameObject.FindGameObjectsWithTag("Player")[0].transform;
 		_ren = transform.GetComponent<SpriteRenderer>();
-		_debugLine = transform.GetComponent<LineRenderer>();
+		//_debugLine = transform.GetComponent<LineRenderer>();
     }
 
     void Update()
@@ -35,95 +44,134 @@ public class EnemyAI : MonoBehaviour
 		if (target == null)
 			return;
 
-		_debugLine.SetPosition (0, enemyPos);
-		_debugLine.SetPosition (1, playerPos);
+        // Debug stuff to draw line of sight to player
+        //_debugLine.SetPosition(0, enemyPos);
+        //_debugLine.SetPosition(1, playerPos);
 
-        playerPos = new Vector2(target.localPosition.x, target.localPosition.y);//player position 
+        playerPos = new Vector2(target.position.x, target.position.y);//player position 
         enemyPos = new Vector2(this.transform.localPosition.x, this.transform.localPosition.y);//enemy position
-		_distanceToPlayer = Vector3.Distance (transform.transform.position, target.transform.position);
+        _distanceToPlayer = Vector3.Distance (transform.transform.position, target.transform.position);
 
-		if(Time.time > _lastDodgeTime + DodgeCooldown)
-			IsShotIncoming ();
-
-		if(_dodging)
-			_ren.color = Color.blue;
-
-		if (!_dodging &&
-		    Vector3.Distance (transform.transform.position, target.transform.position) < AggroDistance &&
-			Vector3.Distance (transform.transform.position, target.transform.position) > ShootDistance) {//move towards if not close by
-			_ren.color = Color.yellow;
-			transform.position = Vector2.MoveTowards (enemyPos, playerPos, 2 * speed * Time.deltaTime);
-		}
-        /*if (Vector3.Distance(transform.transform.position, target.transform.position) < 1.55)//move away if too close 
+        // If we're not dodging or moving
+        if(!_dodging && !_moving)
         {
-            transform.position = Vector2.MoveTowards(enemyPos, playerPos, -1 * speed * Time.deltaTime);
-        }*/
+            // First, check if a shot is incoming.  If it is, this will set _dodging = true
+            if (Time.time > _lastDodgeTime + DodgeCooldown)
+                IsShotIncoming();
 
-		else if (!_dodging &&
-		         Vector3.Distance (transform.transform.position, target.transform.position) <= ShootDistance) {
-			_ren.color = Color.red;
-			if(Time.time > _lastShotTime + ShootCooldown)
-			{
-				_lastShotTime = Time.time;
-				Shoot ();
-			}
-		} else if(!_dodging) {
-			_ren.color = Color.white;
-		}
+            // Next, if we're in AggroDistance
+            if (_distanceToPlayer < AggroDistance)
+            {
+                // If we've gotten too close, move back some
+                /*if (Vector3.Distance(transform.transform.position, target.transform.position) < 1.55)//move away if too close 
+                {
+                    transform.position = Vector2.MoveTowards(enemyPos, playerPos, -1 * speed * Time.deltaTime);
+                }*/
 
-        if (target.position.x > transform.position.x)//rotates enemy to the right if player is to the right  
-        {
-            enemyRotation.x = 180;
-            transform.localRotation = enemyRotation;
+                // If we're not in shot distance, chase
+                if (Vector3.Distance(transform.transform.position, target.transform.position) > ShootDistance)
+                {
+                    _ren.color = Color.yellow;
+                    transform.position = Vector2.MoveTowards(enemyPos, playerPos, 2 * Speed * Time.deltaTime);
+                }
+
+                // If we're in shoot distance
+                else
+                {
+                    _ren.color = Color.red;
+                    // If we haven't shot since the cooldown
+                    if (Time.time > _lastShotTime + ShootCooldown)
+                    {
+                        ShootCooldown = UnityEngine.Random.Range(2f, 4f);
+                        _lastShotTime = Time.time;
+                        Shoot();
+                    }
+                    else if(Time.time > _lastMoveTime + 2f)
+                    {
+                        _lastMoveTime = Time.time;
+                        if (UnityEngine.Random.Range(0, 3) == 0 )
+                        {
+                            _moving = true;
+                            Vector2 moveLocation = new Vector2(UnityEngine.Random.Range(-4f, 4f), UnityEngine.Random.Range(-4f, 4f));
+                            _moveLocation = enemyPos + moveLocation;
+                            StartCoroutine("Move");
+                        }
+                    }
+                }
+            }
+                                    
+            else
+            {
+                _ren.color = Color.white;
+            }
         }
-        if (target.position.x < transform.position.x)//rotates enemy to the left if player is to the left 
+        // If we're dodging
+        else
         {
-            enemyRotation.x = 0;
-            transform.localRotation = enemyRotation;
+            _ren.color = Color.blue;
         }
+        
     }
 
 	void IsShotIncoming()
 	{
-		//Collider2D[] shots = Physics2D.OverlapCircleAll(transform.position, 15f, 1 << LayerMask.NameToLayer("Projectiles"));
-		RaycastHit2D[] shots = Physics2D.RaycastAll (transform.position, Vector2.ClampMagnitude(playerPos - enemyPos, 1.0f), 200f);
-		foreach (RaycastHit2D shot in shots) {
-			if(shot.collider.tag == "Bullet")
+        Vector2[] areaVectors = DirectionCalc.GetEnemyCollisionVectors(Vector2.ClampMagnitude(playerPos - enemyPos, 1.0f));
+        Collider2D[] Shots = Physics2D.OverlapAreaAll (transform.position - (Vector3)areaVectors[0], transform.position - (Vector3)areaVectors[1]);
+
+		foreach (Collider2D shot in Shots) {
+			if(shot.tag == "Bullet")
 			{
-				IProjectile sI = shot.collider.gameObject.GetComponent<IProjectile>();
+				IProjectile sI = shot.gameObject.GetComponent<IProjectile>();
 				if(!sI.GetIsEnemy()) 
 				{
 					_lastDodgeTime = Time.time;
-					_dodging = true;
-					// TODO: Pick random dodge location, make dodge have chance
-					_dodgeDirection = Vector2.right;
-					StartCoroutine("Dodge");
+                    if (UnityEngine.Random.Range(0, 3) == 0  )
+                    {
+                        _dodging = true;
+                        _dodgeDirection = DirectionCalc.GetEnemyDodgeDirection(Vector2.ClampMagnitude((Vector2)shot.transform.position - (Vector2)transform.position, 1.0f));
+
+                        StartCoroutine("Dodge");
+                    }
 				}
 			}
 		}
 	}
 
 	IEnumerator Dodge()
-	{
-		while (Time.time < _lastDodgeTime + .5f)
-		{
-			transform.position = new Vector2 (transform.position.x + (_dodgeDirection.x * 2 * speed * Time.deltaTime),
-			                                                          transform.position.y + (_dodgeDirection.y * 2 * speed * Time.deltaTime));
-			yield return null;
-		}
-		_dodging = false;
+    {
+        while (Time.time < _lastDodgeTime + .3f)
+        {
+            transform.position = new Vector2(transform.position.x + (_dodgeDirection.x * 2 * DodgeSpeed * Time.deltaTime),
+                                                                      transform.position.y + (_dodgeDirection.y * 2 * DodgeSpeed * Time.deltaTime));
+            yield return null;
+        }
+        _dodging = false;
 		StopCoroutine("Dodge");
 	}
 
-	void Shoot() 
+    IEnumerator Move()
+    {
+        while (Vector3.Distance(transform.transform.position, _moveLocation) > 0.2f && Time.time < _lastMoveTime + 1f)
+        {
+            _ren.color = Color.cyan;
+            transform.position = Vector2.MoveTowards(enemyPos, _moveLocation, 2 * Speed * Time.deltaTime);
+
+            yield return null;
+        }
+
+        _moving = false;
+        StopCoroutine("Move");
+    }
+
+    void Shoot() 
 	{
 		GameObject bulletInstance = Instantiate(Projectile);
 		IProjectile bI = bulletInstance.GetComponent<IProjectile>();
 		
 		bI.SetLocation(transform.position);
 		Vector2 dir = Vector2.ClampMagnitude(playerPos - enemyPos, 1.0f);
-		Vector2 offset = new Vector2 ((float)Random.Range (0, 30) / 300f, (float)Random.Range (0, 30) / 300f);
-		bI.SetDirection(Vector2.ClampMagnitude(dir + offset, 1.0f));
+		//Vector2 offset = new Vector2 ((float)UnityEngine.Random.Range (0, 30) / 300f, (float)UnityEngine.Random.Range (0, 30) / 300f);
+		bI.SetDirection(Vector2.ClampMagnitude(dir , 1.0f));
 		bI.SetIsEnemy (true);
 	}
 }
---
+++ Assets/Scripts/GameManagement/StaticHelpers/DirectionCalc.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class DirectionCalc {
+
+    // This returns the enemy's dodge colision check vectors to be used with OverlapAreaAll
+    public static Vector2[] GetEnemyCollisionVectors(Vector2 dir)
+    {
+        float angle = (float)(Math.Atan2(dir.y, dir.x) / (2 * Math.PI) * 360f);
+        return GetEnemyCollisionVectors(angle);
+    }
+
+    public static Vector2[] GetEnemyCollisionVectors(float angle)
+    {
+        Vector2[] vectors = new Vector2[2];
+        if (angle > 45 && angle <= 135)
+        {
+            // Up
+            vectors[0] = new Vector2(-2f, -10f);
+            vectors[1] = new Vector2(2f, -8f);
+        }
+        else if (angle > 135 || angle <= -135)
+        {
+            // Left
+            vectors[0] = new Vector2(10f, -2f);
+            vectors[1] = new Vector2(8f, 2f);
+        }
+        else if (angle < -45 && angle > -135)
+        {
+            // Down
+            vectors[0] = new Vector2(-2f, 10f);
+            vectors[1] = new Vector2(2f, 8f);
+        }
+        else
+        {
+            // Right
+            vectors[0] = new Vector2(-10f, -2f);
+            vectors[1] = new Vector2(-8f, 2f);
+        }
+
+        return vectors;
+	}
+
+    public static Vector2 GetEnemyDodgeDirection(Vector2 dir)
+    {
+        float angle = (float)(Math.Atan2(dir.y, dir.x) / (2 * Math.PI) * 360f);
+        if (angle > 45 && angle <= 135)
+        {
+            // Up
+            if (angle > 90)
+                return Vector2.right;
+            else
+                return Vector2.left;
+        }
+        else if (angle > 135 || angle <= -135)
+        {
+            // Left
+            if (angle < 0)
+                return Vector2.up;
+            else
+                return Vector2.down;
+        }
+        else if (angle < -45 && angle > -135)
+        {
+            // Down
+            if (angle > -90)
+                return Vector2.right;
+            else
+                return Vector2.left;
+        }
+        else
+        {
+            // Right
+            if (angle < 0)
+                return Vector2.up;
+            else
+                return Vector2.down;
+        }
+
+        return Vector2.up;
+    }
+	
+}
